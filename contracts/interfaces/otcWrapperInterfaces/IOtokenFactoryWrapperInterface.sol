062b138ed1ff652c7698f47c2176f311e859a1a6
Spablob <tiagonunespereira@gmail.com>
2023-03-14 18:13:13 +0000

    add pragma 0.8.10 interfaces for wrapper contract

---
+++ contracts/interfaces/otcWrapperInterfaces/IOtokenFactoryWrapperInterface.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.10;
+
+interface IOtokenFactoryWrapperInterface {
+    function createOtoken(
+        address _underlyingAsset,
+        address _strikeAsset,
+        address _collateralAsset,
+        uint256 _strikePrice,
+        uint256 _expiry,
+        bool _isPut
+    ) external returns (address);
+}
