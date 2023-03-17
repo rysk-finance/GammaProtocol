60b0db7b8d1ef6b4f196875e612b57f4e96bcccd
Spablob <tiagonunespereira@gmail.com>
2023-03-17 17:20:05 +0000

    Adjust deployment scripts

--- migrations/3_setup_ownership.js
+++ migrations/3_setup_ownership.js
@@ -4,6 +4,8 @@ const Oracle = artifacts.require('Oracle')
 const MarginPool = artifacts.require('MarginPool')
 const AddressBook = artifacts.require('AddressBook')
 const Controller = artifacts.require('Controller')
+const MarginRequirements = artifacts.require('MarginRequirements.sol')
+const OTCWrapper = artifacts.require('OTCWrapper.sol')
 
 // import config file
 const deploymentConfig = require('./deployment-config.json')
@@ -18,7 +20,9 @@ module.exports = async function(deployer, network, accounts) {
   const whitelist = await Whitelist.deployed()
   const oracle = await Oracle.deployed()
   const pool = await MarginPool.deployed()
+  const requirements = await MarginRequirements.deployed()
   const controller = await Controller.at(await addressbook.getController())
+  const wrapper = await OTCWrapper.at(await addressbook.getOTCWrapper())
 
   // transfer ownership
   await addressbook.transferOwnership(newOwner, {from: deployerAddress})
@@ -26,4 +30,6 @@ module.exports = async function(deployer, network, accounts) {
   await oracle.transferOwnership(newOwner, {from: deployerAddress})
   await pool.transferOwnership(newOwner, {from: deployerAddress})
   await controller.transferOwnership(newOwner, {from: deployerAddress})
+  await requirements.transferOwnership(newOwner, {from: deployerAddress})
+  await wrapper.transferOwnership(newOwner, {from: deployerAddress})
 }
